1782deca17e6649895b813953e76c0b74251c76e
Reuben Cummings <reubano@gmail.com>
2017-01-01 09:40:31 +0300

    Bump to version 0.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='changanya',
-    version='0.3.1',
+    version='0.4.0',
     description='Library of interesting (non-cryptographic) hashes in pure Python.',
     author='reubano',
     author_email='reubano@gmail.com',
